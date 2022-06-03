2667d15ecb7ddaf55e973162bffeb15f2ee7f8f6
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2022-06-03 20:03:16 -0300

    Changes that were not raised in the last commit.

---
+++ MovieApi/Models/ViewModel/DirectorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieApi.Models.ViewModel
+{
+    public class DirectorViewModel
+    {
+        [Key] public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name field is required.")]
+        public string Name { get; set; }
+    }
+}
---
+++ MovieApi/Models/ViewModel/MovieViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models.ViewModel
+{
+    public class MovieViewModel
+    {
+        [Key] public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Title field is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The Title Description is required.")]
+        public string Description { get; set; }
+
+        public virtual DirectorViewModel Director { get; set; }
+        
+        [Required(ErrorMessage = "The Title Genre is required.")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "The Title Duration is required.")]
+        public int Duration { get; set; }
+    }
+}
